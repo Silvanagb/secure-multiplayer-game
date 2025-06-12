068c32d8fb1d0b5dbfb98b4e68975e33b03c9098
Silvanagb <104772778+Silvanagb@users.noreply.github.com>
2025-06-12 14:29:04 -0300

    Update server.js

--- server.js
+++ server.js
@@ -79,5 +79,5 @@ app.get('/', (req, res) => {
 // Inicializar servidor
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log(Server running on port ${PORT});
+  console.log(`Server running on port ${PORT}`);
 });
