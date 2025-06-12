c013677a1fb3d3d43d33cec56db3e08589472d1f
Silvanagb <104772778+Silvanagb@users.noreply.github.com>
2025-06-12 13:14:59 -0300

    Update Collectible.js

--- classes/Collectible.js
+++ classes/Collectible.js
@@ -1,8 +1,8 @@
-export default class Collectible {
-  constructor(id) {
-    this.id = id;
-    this.value = 1;
-    this.x = Math.floor(Math.random() * 500);
-    this.y = Math.floor(Math.random() * 500);
-  }
+function Collectible(id) {
+  this.id = id;
+  this.value = 1;
+  this.x = Math.floor(Math.random() * 500);
+  this.y = Math.floor(Math.random() * 500);
 }
+
+module.exports = Collectible;
